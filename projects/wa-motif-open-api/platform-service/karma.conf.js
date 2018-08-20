7255124e675429211dc81cefa06cdd0cd67465db
Marco Bonati <marco.bonati@vipera.com>
2018-08-20 14:42:14 +0200

    Fixed API path in order to execute automated tests

--- projects/wa-motif-open-api/platform-service/karma.conf.js
+++ projects/wa-motif-open-api/platform-service/karma.conf.js
@@ -15,15 +15,27 @@ module.exports = function (config) {
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    proxies: {
+      "/rest": {
+        "target": "http://ec2-34-209-90-152.us-west-2.compute.amazonaws.com:8080/",
+        "secure": false,
+        'changeOrigin': true
+      },
+      "/oauth": {
+        "target": "http://ec2-34-209-90-152.us-west-2.compute.amazonaws.com:8080/",
+        "secure": false,
+        'changeOrigin': true
+      }
+    },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
+    port: 9877,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_TRACE,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false
