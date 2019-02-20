e4b969d0b49a034c8e36a66ddc22390e036beda0
Marco Tormento <marco.tormento@vipera.com>
2019-02-20 13:12:12 +0100

    Removed old scripts

--- build_all.sh
+++
@@ -1,7 +0,0 @@
-ng build @wa-motif-open-api/platform-service
-ng build @wa-motif-open-api/security-service
-ng build @wa-motif-open-api/app-content-service
-ng build @wa-motif-open-api/configuration-service
-ng build @wa-motif-open-api/oauth2-service
-ng build wa-motif-open-api-kitchensink-module
-ng build
--- build_all_prod.sh
+++
@@ -1,6 +0,0 @@
-ng build @wa-motif-open-api/platform-service  --prod
-ng build @wa-motif-open-api/security-service  --prod
-ng build @wa-motif-open-api/app-content-service --prod
-ng build wa-motif-open-api-kitchensink-module  --prod
-ng build @wa-motif-open-api/configuration-service --prod
-ng build --prod
